3e4341e988a4d884be000f0fdaefcc799f247de3
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-12-23 18:09:13 +0100

    Add detector sweep models for the mask, threshold, label img and MorpoLibJ detectors.

---
+++ src/main/java/fiji/plugin/trackmate/ctc/ui/detectors/LabelImgDetectorSweepModel.java
@@ -0,0 +1,43 @@
+package fiji.plugin.trackmate.ctc.ui.detectors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.ctc.ui.components.BooleanParamSweepModel;
+import fiji.plugin.trackmate.detection.LabeImageDetectorFactory;
+import fiji.plugin.trackmate.detection.ThresholdDetectorFactory;
+
+public class LabelImgDetectorSweepModel extends AbstractSweepModel
+{
+
+	private final BooleanParamSweepModel simplifyContourParam;
+
+	public LabelImgDetectorSweepModel()
+	{
+		this( new BooleanParamSweepModel() );
+	}
+
+	public LabelImgDetectorSweepModel( final BooleanParamSweepModel simplifyContourParam )
+	{
+		this.simplifyContourParam = simplifyContourParam;
+		simplifyContourParam.listeners().add( () -> notifyListeners() );
+	}
+
+	@Override
+	public List< Settings > generateSettings( final Settings base, final int targetChannel )
+	{
+		final List< Settings > list = new ArrayList<>();
+		for ( final Boolean simplifyContour : simplifyContourParam.getRange() )
+		{
+			final Settings s = base.copyOn( base.imp );
+			s.detectorFactory = new LabeImageDetectorFactory<>();
+			final Map< String, Object > ds = s.detectorFactory.getDefaultSettings();
+			ds.put( ThresholdDetectorFactory.KEY_SIMPLIFY_CONTOURS, simplifyContour );
+			s.detectorSettings = ds;
+			list.add( s );
+		}
+		return list;
+	}
+}
---
+++ src/main/java/fiji/plugin/trackmate/ctc/ui/detectors/MaskDetectorSweepModel.java
@@ -0,0 +1,42 @@
+package fiji.plugin.trackmate.ctc.ui.detectors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.ctc.ui.components.BooleanParamSweepModel;
+import fiji.plugin.trackmate.detection.MaskDetectorFactory;
+
+public class MaskDetectorSweepModel extends AbstractSweepModel
+{
+
+	private final BooleanParamSweepModel simplifyContourParam;
+
+	public MaskDetectorSweepModel()
+	{
+		this( new BooleanParamSweepModel() );
+	}
+
+	public MaskDetectorSweepModel( final BooleanParamSweepModel simplifyContourParam )
+	{
+		this.simplifyContourParam = simplifyContourParam;
+		simplifyContourParam.listeners().add( () -> notifyListeners() );
+	}
+
+	@Override
+	public List< Settings > generateSettings( final Settings base, final int targetChannel )
+	{
+		final List< Settings > list = new ArrayList<>();
+		for ( final Boolean simplifyContour : simplifyContourParam.getRange() )
+		{
+			final Settings s = base.copyOn( base.imp );
+			s.detectorFactory = new MaskDetectorFactory<>();
+			final Map< String, Object > ds = s.detectorFactory.getDefaultSettings();
+			ds.put( MaskDetectorFactory.KEY_SIMPLIFY_CONTOURS, simplifyContour );
+			s.detectorSettings = ds;
+			list.add( s );
+		}
+		return list;
+	}
+}
