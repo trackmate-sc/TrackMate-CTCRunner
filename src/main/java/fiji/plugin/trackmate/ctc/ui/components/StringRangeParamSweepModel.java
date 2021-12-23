4baf84a7c4080ce85af14f44ffe8a07c19f00797
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-12-23 18:07:12 +0100

    Add a sweep model for a range of strings.

---
+++ src/main/java/fiji/plugin/trackmate/ctc/ui/components/StringRangeParamSweepModel.java
@@ -0,0 +1,55 @@
+package fiji.plugin.trackmate.ctc.ui.components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class StringRangeParamSweepModel extends AbstractParamSweepModel< String >
+{
+
+	protected final List< String > stringList = new ArrayList<>();
+
+	@Override
+	public StringRangeParamSweepModel paramName( final String paramName )
+	{
+		return ( StringRangeParamSweepModel ) super.paramName( paramName );
+	}
+
+	public StringRangeParamSweepModel add( final String string )
+	{
+		stringList.add( string );
+		notifyListeners();
+		return this;
+	}
+
+	public StringRangeParamSweepModel removeLast()
+	{
+		return remove( stringList.size() - 1 );
+	}
+
+	public StringRangeParamSweepModel remove( final int index )
+	{
+		if ( stringList.size() < 2 || index < 0 || index >= stringList.size() )
+			return this;
+
+		stringList.remove( index );
+		notifyListeners();
+		return this;
+	}
+
+	@Override
+	public String[] getRange()
+	{
+		return stringList.toArray( new String[] {} );
+	}
+
+	@Override
+	public String toString()
+	{
+		final StringBuilder str = new StringBuilder();
+		str.append( String.format( "%s:\n", paramName ) );
+		int index = 0;
+		for ( final String string : stringList )
+			str.append( String.format( " - %2d: %s\n", index++, string ) );
+		return str.toString();
+	}
+}
