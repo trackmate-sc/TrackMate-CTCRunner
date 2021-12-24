00ff79d401e40ccad1fd85e7bbfcba92972d56c8
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-12-24 13:16:01 +0100

    Finish the panel to edit a string list.

--- src/main/java/fiji/plugin/trackmate/ctc/ui/components/StringRangeParamSweepPanel.java
+++ src/main/java/fiji/plugin/trackmate/ctc/ui/components/StringRangeParamSweepPanel.java
@@ -1,5 +1,6 @@
 package fiji.plugin.trackmate.ctc.ui.components;
 
+import static fiji.plugin.trackmate.gui.Fonts.FONT;
 import static fiji.plugin.trackmate.gui.Fonts.SMALL_FONT;
 import static fiji.plugin.trackmate.gui.Icons.ADD_ICON;
 import static fiji.plugin.trackmate.gui.Icons.REMOVE_ICON;
@@ -7,18 +8,25 @@ import static fiji.plugin.trackmate.gui.Icons.REMOVE_ICON;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
+
+import com.itextpdf.text.Font;
 
 import fiji.plugin.trackmate.util.EverythingDisablerAndReenabler;
 import fiji.plugin.trackmate.util.FileChooser;
@@ -32,8 +40,6 @@ public class StringRangeParamSweepPanel extends JPanel
 
 	private final List< StringPanel > stringPanels = new ArrayList<>();
 
-	private final List< Component > struts = new ArrayList<>();
-
 	private final JPanel allStringPanels;
 
 	private final StringRangeParamSweepModel values;
@@ -42,70 +48,64 @@ public class StringRangeParamSweepPanel extends JPanel
 
 	private final boolean showBrowse;
 
+	private final JPanel panelButton;
+
 	/*
 	 * CONSTRUCTOR
 	 */
 
 	public StringRangeParamSweepPanel( final StringRangeParamSweepModel val )
 	{
-		this.values = val;
 		this.showBrowse = val.isFile();
 		this.enabler = new EverythingDisablerAndReenabler( this, new Class[] { JLabel.class } );
 
 		this.setLayout( new BorderLayout() );
-		setPreferredSize( new Dimension( 270, 500 ) );
+		setPreferredSize( new Dimension( 270, 150 ) );
 
 		final JPanel topPanel = new JPanel();
+		final JLabel lblName = new JLabel( val.paramName );
+		lblName.setFont( FONT.deriveFont( Font.BOLD ) );
+		topPanel.add( lblName );
 		add( topPanel, BorderLayout.NORTH );
-		topPanel.setLayout( new BorderLayout( 0, 0 ) );
 
-		final JScrollPane scrollPaneThresholds = new JScrollPane();
-		this.add( scrollPaneThresholds, BorderLayout.CENTER );
-		scrollPaneThresholds.setPreferredSize( new java.awt.Dimension( 250, 389 ) );
-		scrollPaneThresholds.setHorizontalScrollBarPolicy( ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER );
-		scrollPaneThresholds.setVerticalScrollBarPolicy( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+		final JScrollPane scrollPane = new JScrollPane();
+		this.add( scrollPane, BorderLayout.CENTER );
+		scrollPane.setPreferredSize( new java.awt.Dimension( 250, 389 ) );
+		scrollPane.setHorizontalScrollBarPolicy( ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER );
+		scrollPane.setVerticalScrollBarPolicy( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+		scrollPane.setBorder( null );
+		scrollPane.getVerticalScrollBar().setUnitIncrement( 16 );
 
 		allStringPanels = new JPanel();
 		final BoxLayout jPanelAllThresholdsLayout = new BoxLayout( allStringPanels, BoxLayout.Y_AXIS );
 		allStringPanels.setLayout( jPanelAllThresholdsLayout );
-		scrollPaneThresholds.setViewportView( allStringPanels );
+		scrollPane.setViewportView( allStringPanels );
 
-		final JPanel bottomPanel = new JPanel();
-		bottomPanel.setLayout( new BorderLayout() );
-		this.add( bottomPanel, BorderLayout.SOUTH );
-
-		final JPanel buttonsPanel = new JPanel();
-		bottomPanel.add( buttonsPanel, BorderLayout.NORTH );
-		final BoxLayout jPanelButtonsLayout = new BoxLayout( buttonsPanel, javax.swing.BoxLayout.X_AXIS );
-		buttonsPanel.setLayout( jPanelButtonsLayout );
-		buttonsPanel.setPreferredSize( new java.awt.Dimension( 270, 22 ) );
-		buttonsPanel.setSize( 270, 25 );
-		buttonsPanel.setMaximumSize( new java.awt.Dimension( 32767, 25 ) );
-
-		buttonsPanel.add( Box.createHorizontalStrut( 5 ) );
-		final JButton btnAddFilter = new JButton();
-		buttonsPanel.add( btnAddFilter );
-		btnAddFilter.setIcon( ADD_ICON );
-		btnAddFilter.setFont( SMALL_FONT );
-		btnAddFilter.setPreferredSize( new java.awt.Dimension( 24, 24 ) );
-		btnAddFilter.setSize( 24, 24 );
-		btnAddFilter.setMinimumSize( new java.awt.Dimension( 24, 24 ) );
-
-		buttonsPanel.add( Box.createHorizontalGlue() );
-		buttonsPanel.add( Box.createHorizontalStrut( 5 ) );
+		final JButton btnAdd = new JButton();
+		btnAdd.setIcon( ADD_ICON );
+		btnAdd.setFont( SMALL_FONT );
+		btnAdd.setPreferredSize( new java.awt.Dimension( 24, 24 ) );
+		btnAdd.setSize( 24, 24 );
+		btnAdd.setMinimumSize( new java.awt.Dimension( 24, 24 ) );
+		panelButton = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
+		panelButton.add( btnAdd );
 
 		/*
 		 * Default values.
 		 */
 
-		for ( final String string : values.getRange() )
+		for ( final String string : val.getRange() )
 			addStringPanel( string );
+
+		// We assign the field only now so that the lines above do not result in
+		// adding the strings to the model a second time.
+		this.values = val;
 
 		/*
 		 * Listeners & co.
 		 */
 
-		btnAddFilter.addActionListener( e -> addStringPanel() );
+		btnAdd.addActionListener( e -> addStringPanel() );
 	}
 
 	public void addStringPanel()
@@ -121,17 +121,20 @@ public class StringRangeParamSweepPanel extends JPanel
 
 	public void addStringPanel( final StringPanel panel )
 	{
+		allStringPanels.remove( panelButton );
+
 		final Component strut = Box.createVerticalStrut( 5 );
-		struts.add( strut );
 		stringPanels.add( panel );
 		allStringPanels.add( panel );
 		allStringPanels.add( strut );
+		allStringPanels.add( panelButton );
 
 		if ( stringPanels.size() > 1 )
 			stringPanels.forEach( sp -> sp.btnRemove.setVisible( true ) );
 
-		final int id = stringPanels.size() - 1;
-		panel.btnRemove.addActionListener( e -> removeStringPanel( id ) );
+		panel.btnRemove.addActionListener( e -> removeStringPanel( panel, strut ) );
+		if ( values != null )
+			values.add( panel.tfStr.getText() );
 
 		allStringPanels.revalidate();
 	}
@@ -140,12 +143,17 @@ public class StringRangeParamSweepPanel extends JPanel
 	 * PRIVATE METHODS
 	 */
 
-	private void removeStringPanel( final int id )
+	private void removeStringPanel( final StringPanel stringPanel, final Component strut )
 	{
-		final StringPanel stringPanel = stringPanels.remove( id );
-		final Component strut = struts.remove( id );
+		final int id = stringPanels.indexOf( stringPanel );
+		values.remove( id );
+		stringPanels.remove( stringPanel );
+
+		if ( stringPanels.size() < 2 )
+			stringPanels.forEach( sp -> sp.btnRemove.setVisible( false ) );
 		allStringPanels.remove( strut );
 		allStringPanels.remove( stringPanel );
+		allStringPanels.revalidate();
 		allStringPanels.repaint();
 	}
 
@@ -177,6 +185,7 @@ public class StringRangeParamSweepPanel extends JPanel
 			add( Box.createHorizontalStrut( 5 ) );
 			tfStr = new JTextField( str );
 			tfStr.setFont( SMALL_FONT );
+			add( tfStr );
 
 			if ( showBrowse )
 			{
@@ -184,8 +193,31 @@ public class StringRangeParamSweepPanel extends JPanel
 				final JButton btnBrowse = new JButton( "Browse" );
 				btnBrowse.setFont( SMALL_FONT );
 				btnBrowse.addActionListener( e -> browse() );
+				add( btnBrowse );
 			}
 
+			setMinimumSize( new java.awt.Dimension( 24, 24 ) );
+			setPreferredSize( new java.awt.Dimension( 100, 24 ) );
+			setMaximumSize( new java.awt.Dimension( 6000, 30 ) );
+			setBorder( BorderFactory.createEmptyBorder( 2, 5, 2, 5 ) );
+
+			// Listeners.
+			fiji.plugin.trackmate.gui.GuiUtils.selectAllOnFocus( tfStr );
+
+			final ActionListener al = e -> {
+				final int id = stringPanels.indexOf( this );
+				values.set( id, tfStr.getText() );
+			};
+			tfStr.addActionListener( al );
+			final FocusAdapter fa = new FocusAdapter()
+			{
+				@Override
+				public void focusLost( final java.awt.event.FocusEvent e )
+				{
+					al.actionPerformed( null );
+				}
+			};
+			tfStr.addFocusListener( fa );
 		}
 
 		private void browse()
@@ -199,7 +231,7 @@ public class StringRangeParamSweepPanel extends JPanel
 						null,
 						"Browse to a file",
 						DialogType.LOAD,
-						SelectionMode.FILES_ONLY );
+						SelectionMode.FILES_AND_DIRECTORIES );
 				if ( file != null )
 					tfStr.setText( file.getAbsolutePath() );
 			}
@@ -208,5 +240,23 @@ public class StringRangeParamSweepPanel extends JPanel
 				enabler.reenable();
 			}
 		}
+	}
+
+	public static void main( final String[] args )
+	{
+		final StringRangeParamSweepModel model = new StringRangeParamSweepModel()
+				.paramName( "Test string list" )
+				.isFile( true )
+				.add( "Test 1" )
+				.add( "Rather long string that should be able to show, maybe, how the UI behaves against such long strings. That's it." )
+				.add( "With a ut8 char: €μ" );
+		model.listeners().add( () -> System.out.println( model.toString() ) );
+		
+		final StringRangeParamSweepPanel panel = new StringRangeParamSweepPanel( model );
+		final JFrame frame = new JFrame();
+		frame.getContentPane().add( panel );
+		frame.pack();
+		frame.setLocationRelativeTo( null );
+		frame.setVisible( true );
 	}
 }
