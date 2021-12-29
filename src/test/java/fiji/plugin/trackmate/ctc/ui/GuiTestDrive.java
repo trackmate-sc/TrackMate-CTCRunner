47da5e191a04d9226a7800e9765316fedbed61cf
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-12-29 21:19:30 +0100

    Tweak the test drive.

--- src/test/java/fiji/plugin/trackmate/ctc/ui/GuiTestDrive.java
+++ src/test/java/fiji/plugin/trackmate/ctc/ui/GuiTestDrive.java
@@ -1,5 +1,7 @@
 package fiji.plugin.trackmate.ctc.ui;
 
+import java.io.File;
+
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -14,6 +16,11 @@ public class GuiTestDrive
 		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
 		final ImageJ ij = new ImageJ();
 		ij.launch( args );
-		new TrackMateParameterSweepPlugin().run( "samples/MAX_Merged.tif" );
+		final String rootFolder = "D:\\Projects\\JYTinevez\\TrackMate-StarDist\\CTCMetrics\\CellMigration";
+		final String sourceImagePath = new File( rootFolder, "CellMigration.tif" ).getAbsolutePath();
+		final String groundTruthPath = new File( rootFolder, "02_GT" ).getAbsolutePath();
+		final TrackMateParameterSweepPlugin plugin = new TrackMateParameterSweepPlugin();
+		plugin.run( sourceImagePath );
+		plugin.getController().setGroundTruthPath( groundTruthPath );
 	}
 }
