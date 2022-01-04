52125d6435f2d6f9ce70e2352c55425c48f90165
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2022-01-04 18:33:52 +0100

    Work around the StarDist detector having no parameters
    but still requiring to return one Settings object.

--- src/main/java/fiji/plugin/trackmate/ctc/ui/detectors/optional/StarDistDetector.java
+++ src/main/java/fiji/plugin/trackmate/ctc/ui/detectors/optional/StarDistDetector.java
@@ -1,5 +1,12 @@
 package fiji.plugin.trackmate.ctc.ui.detectors.optional;
 
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+
+import fiji.plugin.trackmate.Settings;
 import fiji.plugin.trackmate.ctc.ui.components.DoubleParamSweepModel;
 import fiji.plugin.trackmate.ctc.ui.components.NumberParamSweepModel.RangeType;
 import fiji.plugin.trackmate.ctc.ui.components.StringRangeParamSweepModel;
@@ -10,12 +17,29 @@ import fiji.plugin.trackmate.stardist.StarDistDetectorFactory;
 public class StarDistDetector
 {
 
+	private static final class StardistDetectorModel extends DetectorSweepModel
+	{
+
+		protected StardistDetectorModel()
+		{
+			super( StarDistDetectorFactory.NAME, Collections.emptyMap(), new StarDistDetectorFactory<>() );
+		}
+
+		@Override
+		public Iterator< Settings > iterator( final Settings base, final int targetChannel )
+		{
+			final Settings s = base.copyOn( base.imp );
+			final Map< String, Object > ds = factory.getDefaultSettings();
+			ds.put( KEY_TARGET_CHANNEL, targetChannel );
+			s.detectorFactory = factory.copy();
+			s.detectorSettings = ds;
+			return Collections.singleton( s ).iterator();
+		}
+	}
+
 	public static DetectorSweepModel stardistDetectorModel()
 	{
-		return DetectorSweepModel.create()
-				.name( StarDistDetectorFactory.NAME )
-				.factory( new StarDistDetectorFactory<>() )
-				.get();
+		return new StardistDetectorModel();
 	}
 
 	public static DetectorSweepModel stardistCustomDetectorModel()
