92d5a1858444448d70d9aed2567e3548946abe21
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2022-01-10 17:19:52 +0100

    Update the test-drives.

--- src/test/java/fiji/plugin/trackmate/ctc/ui/CrawlerResultsUITestDrive.java
+++ src/test/java/fiji/plugin/trackmate/ctc/ui/CrawlerResultsUITestDrive.java
@@ -2,12 +2,10 @@ package fiji.plugin.trackmate.ctc.ui;
 
 import java.io.IOException;
 
-import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import fiji.plugin.trackmate.Logger;
-import fiji.plugin.trackmate.ctc.CTCResultsCrawler;
+import fiji.plugin.trackmate.ctc.TrackMateParameterSweepResultsPlugin;
 
 public class CrawlerResultsUITestDrive
 {
@@ -17,15 +15,7 @@ public class CrawlerResultsUITestDrive
 		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
 //			final String resultsFolder = "/Users/tinevez/Projects/JYTinevez/TrackMate-StarDist/CellMigration/";
 		final String resultsFolder = "D:\\Projects\\JYTinevez\\TrackMate-StarDist\\CTCMetrics\\CellMigration";
-		final CTCResultsCrawler crawler = new CTCResultsCrawler( Logger.DEFAULT_LOGGER );
 
-		final CrawlerResultsPanel panel = new CrawlerResultsPanel( crawler );
-		final JFrame frame = new JFrame( "TrackMate parameter sweep results" );
-		frame.getContentPane().add( panel );
-		frame.pack();
-		frame.setLocationRelativeTo( null );
-		frame.setVisible( true );
-
-		crawler.crawl( resultsFolder );
+		new TrackMateParameterSweepResultsPlugin().run( resultsFolder );
 	}
 }
--- src/test/java/fiji/plugin/trackmate/ctc/ui/GuiTestDrive.java
+++ src/test/java/fiji/plugin/trackmate/ctc/ui/GuiTestDrive.java
@@ -16,9 +16,24 @@ public class GuiTestDrive
 		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
 		final ImageJ ij = new ImageJ();
 		ij.launch( args );
-		final String rootFolder = "D:\\Projects\\JYTinevez\\TrackMate-StarDist\\CTCMetrics\\CellMigration";
-		final String sourceImagePath = new File( rootFolder, "CellMigration.tif" ).getAbsolutePath();
-		final File groundTruthPath = new File( rootFolder, "02_GT" );
+
+//		final String rootFolder = "D:\\Projects\\JYTinevez\\TrackMate-StarDist\\CTCMetrics\\TCells\\JY";
+//		final String sourceImagePath = new File( rootFolder, "TCellsMigration.tif" ).getAbsolutePath();
+//		final File groundTruthPath = new File( rootFolder, "01_GT" );
+
+		// final String rootFolder =
+		// "D:\\Projects\\JYTinevez\\TrackMate-StarDist\\CTCMetrics\\CellMigration";
+//		final String sourceImagePath = new File( rootFolder, "CellMigration.tif" ).getAbsolutePath();
+//		final File groundTruthPath = new File( rootFolder, "02_GT" );
+
+		final String rootFolder = "D:\\Projects\\JYTinevez\\TrackMate-StarDist\\CTCMetrics\\NMeningitidis";
+		final String sourceImagePath = new File( rootFolder, "NeisseriaMeningitidisGrowth.tif" ).getAbsolutePath();
+		final File groundTruthPath = new File( rootFolder, "01_GT" );
+
+//		final String rootFolder = "D:\\Projects\\JYTinevez\\TrackMate-StarDist\\CTCMetrics\\";
+//		final String sourceImagePath = new File( rootFolder, "NMeningitidis/NeisseriaMeningitidisGrowth.tif" ).getAbsolutePath();
+//		final File groundTruthPath = new File( rootFolder, "01_GT" );
+
 		final TrackMateParameterSweepPlugin plugin = new TrackMateParameterSweepPlugin();
 		plugin.run( sourceImagePath + ", " + groundTruthPath );
 
