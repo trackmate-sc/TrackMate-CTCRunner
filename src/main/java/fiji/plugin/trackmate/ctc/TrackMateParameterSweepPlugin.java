1497c9efdb6a01327b4019de9cdc9045f2b5ecbd
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2022-02-12 20:01:23 +0100

    Rework the plugin launchers.
    - Remember parameters between sessions.
    - Offer to browse to an image if none is open.
    - Tweaked the look, texts and name.

--- src/main/java/fiji/plugin/trackmate/ctc/TrackMateParameterSweepPlugin.java
+++ src/main/java/fiji/plugin/trackmate/ctc/TrackMateParameterSweepPlugin.java
@@ -25,17 +25,26 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.scijava.prefs.PrefService;
+import org.scijava.util.VersionUtils;
+
 import fiji.plugin.trackmate.ctc.ui.ParameterSweepController;
 import fiji.plugin.trackmate.gui.Fonts;
 import fiji.plugin.trackmate.gui.GuiUtils;
 import fiji.plugin.trackmate.gui.Icons;
+import fiji.plugin.trackmate.util.TMUtils;
 import fiji.util.gui.GenericDialogPlus;
 import ij.IJ;
 import ij.ImagePlus;
+import ij.WindowManager;
 import ij.plugin.PlugIn;
 
 public class TrackMateParameterSweepPlugin implements PlugIn
 {
+
+	private static final String GT_FOLDER_KEY = "GT_FOLDER";
+
+	private static final String IMAGE_PATH_KEY = "IMAGE_PATH";
 
 	private ParameterSweepController controller;
 
@@ -60,12 +69,32 @@ public class TrackMateParameterSweepPlugin implements PlugIn
 			}
 		}
 
-		final GenericDialogPlus dialog = new GenericDialogPlus( "TrackMate parameter sweep startup" );
+		final PrefService prefService = TMUtils.getContext().getService( PrefService.class );
+
+		// Do we have an image opened?
+		final boolean impOpen = WindowManager.getImageCount() > 0;
+
+		final GenericDialogPlus dialog = new GenericDialogPlus( "TrackMate-Helper startup" );
 		dialog.setIconImage( Icons.TRACKMATE_ICON.getImage() );
 
-		dialog.addMessage( "TrackMate parameter sweep.", Fonts.BIG_FONT );
+		dialog.addImage( Icons.TRACKMATE_ICON );
+		dialog.addToSameRow();
+		dialog.addMessage( "TrackMate-Helper", Fonts.BIG_FONT );
+		dialog.addMessage( "v" + VersionUtils.getVersion( TrackMateParameterSweepPlugin.class ), Fonts.SMALL_FONT );
+		dialog.addMessage( "__________________________________________________________" );
+
 		dialog.addMessage( "Please select an image to run tracking on." );
-		dialog.addImageChoice( "Source image", "" );
+
+		if ( impOpen )
+			dialog.addImageChoice( "Source image:", "" );
+		else
+		{
+			final String lastUsedImagePathFolder = prefService.get( getClass(), IMAGE_PATH_KEY, System.getProperty( "user.home" ) );
+			dialog.addMessage( "Path to the source image:" );
+			dialog.addFileField( "", lastUsedImagePathFolder, 50 );
+		}
+
+		dialog.addMessage( "__________________________________________________________" );
 
 		dialog.addMessage( "Please browse to the folder in which the tracking ground-truth is stored." );
 		dialog.addMessage( "The results will be written in the parent of this folder." );
@@ -73,15 +102,36 @@ public class TrackMateParameterSweepPlugin implements PlugIn
 				+ "Ulman, et al (2017). An objective comparison of cell-tracking algorithms.\n"
 				+ "Nature Methods, 14(12), 1141–1152. https://doi.org/10.1038/nmeth.4473\n"
 				+ "for details." );
-		dialog.addMessage( "Path to tracking ground-truth for source image" );
-		dialog.addDirectoryField( "", System.getProperty( "user.home" ), 50 );
+		dialog.addMessage( "Path to tracking ground-truth folder:" );
+		
+		final String lastUsedGtFolder = prefService.get( getClass(), GT_FOLDER_KEY, System.getProperty( "user.home" ) );
+		dialog.addDirectoryField( "", lastUsedGtFolder, 50 );
 
 		dialog.showDialog();
 		if ( dialog.wasCanceled() )
 			return;
 
-		final ImagePlus imp = dialog.getNextImage();
+
+		final ImagePlus imp;
+		if ( impOpen )
+		{
+			imp = dialog.getNextImage();
+		}
+		else
+		{
+			final String imagePath = dialog.getNextString();
+			imp = IJ.openImage( imagePath );
+			if ( imp == null )
+			{
+				IJ.error( "TrackMate-Helper", "Could not open image file " + imagePath );
+				return;
+			}
+			prefService.put( getClass(), IMAGE_PATH_KEY, imagePath );
+			imp.show();
+		}
+
 		final String gtPath = dialog.getNextString();
+		prefService.put( getClass(), GT_FOLDER_KEY, gtPath );
 
 		controller = new ParameterSweepController( imp, gtPath );
 		controller.show();
