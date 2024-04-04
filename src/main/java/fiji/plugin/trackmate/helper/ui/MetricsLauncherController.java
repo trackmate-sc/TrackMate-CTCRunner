a3dce7c981bbf728213577635fca8721ec79f0df
Jean-Yves Tinevez <tinevez@pasteur.fr>
2024-04-04 15:10:07 +0200

    Working metric computation tool.

--- src/main/java/fiji/plugin/trackmate/helper/ui/MetricsLauncherController.java
+++ src/main/java/fiji/plugin/trackmate/helper/ui/MetricsLauncherController.java
@@ -22,11 +22,16 @@
 package fiji.plugin.trackmate.helper.ui;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import org.apache.commons.io.FilenameUtils;
 import org.scijava.Cancelable;
+
+import com.opencsv.CSVWriter;
 
 import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.Model;
@@ -34,11 +39,13 @@ import fiji.plugin.trackmate.Settings;
 import fiji.plugin.trackmate.TrackMate;
 import fiji.plugin.trackmate.gui.Icons;
 import fiji.plugin.trackmate.helper.MetricsRunner;
+import fiji.plugin.trackmate.helper.TrackingMetrics;
 import fiji.plugin.trackmate.helper.TrackingMetricsType;
 import fiji.plugin.trackmate.helper.ctc.CTCTrackingMetricsType;
 import fiji.plugin.trackmate.helper.spt.SPTTrackingMetricsType;
 import fiji.plugin.trackmate.io.TmXmlReader;
 import fiji.plugin.trackmate.util.EverythingDisablerAndReenabler;
+import ij.ImagePlus;
 import net.imagej.ImageJ;
 
 public class MetricsLauncherController implements Cancelable
@@ -75,7 +82,8 @@ public class MetricsLauncherController implements Cancelable
 						final String gtPath = gui.tfGTPath.getText();
 						final String inputPath = gui.tfInputPath.getText();
 						final double maxDist = gui.getSPTMaxPairingDistance();
-						computeMetrics( ctcSelected, gtPath, inputPath, maxDist );
+						final String units = gui.getUnits();
+						computeMetrics( ctcSelected, gtPath, inputPath, maxDist, units );
 					}
 					finally
 					{
@@ -87,7 +95,7 @@ public class MetricsLauncherController implements Cancelable
 		frame.setVisible( true );
 	}
 
-	private void computeMetrics( final boolean ctcSelected, final String gtPath, final String inputPath, final double maxDist )
+	private void computeMetrics( final boolean ctcSelected, final String gtPath, final String inputPath, final double maxDist, final String units )
 	{
 		isCanceled = false;
 
@@ -99,15 +107,47 @@ public class MetricsLauncherController implements Cancelable
 		else
 			saveFolder = input.getParent();
 
-		logger.log( "Performing tracking metrics measurements.\n"
-				+ " - Tracking metrics type: " + ( ctcSelected ? "Cell Tracking Challenge" : "Single-Particle Tracking Challenge" ) + "\n"
-				+ " - Ground-truth file: " + gtPath +"\n" );
-		
 		// Prepare the runner.
 		final TrackingMetricsType type = ctcSelected
 				? new CTCTrackingMetricsType()
-				: new SPTTrackingMetricsType( maxDist );
+				: new SPTTrackingMetricsType( maxDist, units );
 
+		// Prepare the CSV file.
+		final String gtName = FilenameUtils.removeExtension( new File( gtPath ).getName() );
+		final String csvFileName = type.csvSuffix() + "_" + gtName + ".csv";
+		final File csvFile = new File( saveFolder, csvFileName );
+		
+		// Echo info
+		logger.log( "Performing tracking metrics measurements.\n"
+				+ " - Tracking metrics type: " + ( ctcSelected ? "Cell Tracking Challenge" : "Single-Particle Tracking Challenge" ) + "\n"
+				+ " - Ground-truth file: " + gtPath +"\n"
+				+ " - Saving metrics to CSV file: " + csvFile + "\n" );
+		final StringBuilder str = new StringBuilder();
+		for ( int i = 0; i < type.metrics().size(); i++ )
+			str.append( " - " + type.metrics().get( i ).key + ": " + type.metrics().get( i ).description + '\n' );
+		logger.log( str.toString() );
+		
+		// Save CSV header.
+		final String[] csvHeader = new String[ 1 + type.metrics().size() ];
+		csvHeader[ 0 ] = "File";
+		for ( int i = 0; i < type.metrics().size(); i++ )
+			csvHeader[ i + 1 ] = type.metrics().get( i ).key;
+
+		try (CSVWriter csvWriter = new CSVWriter( new FileWriter( csvFile, false ),
+				CSVWriter.DEFAULT_SEPARATOR,
+				CSVWriter.NO_QUOTE_CHARACTER,
+				CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+				CSVWriter.DEFAULT_LINE_END ))
+		{
+			csvWriter.writeNext( csvHeader );
+		}
+		catch ( final IOException e )
+		{
+			logger.error( "Error saving results to CSV file " + csvFile + ":\n" + e.getMessage() );
+			return;
+		}
+
+		// Process input.
 		try
 		{
 			final MetricsRunner runner = type.runner( gtPath, saveFolder );
@@ -124,12 +164,12 @@ public class MetricsLauncherController implements Cancelable
 						logger.log( "Canceled" );
 						return;
 					}
-					process( xmlFile, runner );
+					process( xmlFile, runner, csvFile );
 				}
 			}
 			else
 			{
-				process( input, runner );
+				process( input, runner, csvFile );
 			}
 			logger.log( "____________________________________\nDone.\n" );
 		}
@@ -139,20 +179,45 @@ public class MetricsLauncherController implements Cancelable
 		}
 	}
 
-	private void process( final File xmlFile, final MetricsRunner runner )
+	private void process( final File xmlFile, final MetricsRunner runner, final File csvFile )
 	{
 		logger.log( "____________________________________\nProcessing file " + xmlFile + "\n" );
 		try
 		{
 			final TmXmlReader reader = new TmXmlReader( xmlFile );
 			final Model model = reader.getModel();
-			/*
-			 * Warning! We don't load the image for the sake of time. We will
-			 * then get a default CSV name for the output.
-			 */
-			final Settings settings = reader.readSettings( null );
+			final ImagePlus imp = reader.readImage();
+			final Settings settings = reader.readSettings( imp );
 			final TrackMate trackmate = new TrackMate( model, settings );
-			runner.performMetricsMeasurements( trackmate, Double.NaN, Double.NaN );
+			final TrackingMetrics metrics = runner.performMetricsMeasurements( trackmate );
+
+			logger.log( metrics.toString() );
+
+			final double[] values = metrics.toArray();
+			final String[] line = new String[ 1 + values.length ];
+			line[ 0 ] = xmlFile.getName();
+			for ( int i = 0; i < values.length; i++ )
+				line[ i + 1 ] = "" + values[ i ];
+
+			try (CSVWriter csvWriter = new CSVWriter( new FileWriter( csvFile, true ),
+					CSVWriter.DEFAULT_SEPARATOR,
+					CSVWriter.NO_QUOTE_CHARACTER,
+					CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+					CSVWriter.DEFAULT_LINE_END ))
+			{
+				csvWriter.writeNext( line );
+			}
+			catch ( final IOException e )
+			{
+				logger.error( "Error saving results to CSV file " + csvFile + ":\n" + e.getMessage() );
+			}
+
+			// Loop
+			if ( imp != null )
+			{
+				imp.changes = false;
+				imp.close();
+			}
 		}
 		catch ( final Exception ex )
 		{
